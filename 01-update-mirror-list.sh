057d4a2104b1896c896850ac344e82d531d0a597
terlim <terlim@yandex.ru>
2019-12-04 11:57:05 +0300

    fix PATH copy reflector.conf

--- 01-update-mirror-list.sh
+++ 01-update-mirror-list.sh
@@ -15,6 +15,6 @@ sudo pacman -S reflector --noconfirm --needed
 
 yaourt reflector-timer
 
-sudo cp /src/reflector.conf /usr/share/reflector-timer/reflector.conf
+sudo cp ./src/reflector.conf /usr/share/reflector-timer/reflector.conf
 sudo systemctl start reflector.timer
 sudo pacman -Syy
