5beb1c44b55bddc5142886f1e99d48512dd267a4
terlim <terlim@yandex.ru>
2019-12-04 16:40:28 +0300

    add arch theme

--- 12-install-light-dm.sh
+++ 12-install-light-dm.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 #
 #
-sudo pacman -S lightdm
-sudo pacman -S lightdm-gtk-greeter lightdm-gtk-greeter-settings lightdm-webkit-theme-litarvan
+sudo pacman -S lightdm --noconfirm --needed
+sudo pacman -S lightdm-gtk-greeter lightdm-gtk-greeter-settings  --noconfirm --needed
+
 sudo systemctl enable lightdm.service
---
+++ 13-arch-theme.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+sudo pacman -S capitaine-cursors
+sudo pacman -S arc-gtk-theme
+yaourt ardis-icon-theme-circle
