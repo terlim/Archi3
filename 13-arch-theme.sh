723bb5233752a50127f894611412cdec2b2dc87f
terlim <terlim@yandex.ru>
2019-12-05 20:38:47 +0300

    fix

--- 13-arch-theme.sh
+++ 13-arch-theme.sh
@@ -3,4 +3,6 @@ sudo pacman -S capitaine-cursors --noconfirm --needed
 sudo pacman -S arc-gtk-theme --noconfirm --needed
 yaourt ardis-icon-theme-circle --noconfirm --needed
 yaourt otf-san-francisco-pro --noconfirm --needed
+yaourt nerd-fonts-melso
+
 
