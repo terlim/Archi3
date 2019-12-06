888f97cf19499aa875cf9fe9e7898427c75726d8
terlim <terlim@yandex.ru>
2019-12-06 11:37:56 +0300

    15

--- 12-install-light-dm.sh
+++ 12-install-light-dm.sh
@@ -3,5 +3,5 @@
 #
 sudo pacman -S lightdm --noconfirm --needed
 sudo pacman -S lightdm-gtk-greeter lightdm-gtk-greeter-settings  --noconfirm --needed
-
+cp ./src/.xsession $HOME/.xsession
 sudo systemctl enable lightdm.service
--- 13-arch-theme.sh
+++ 13-arch-theme.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 sudo pacman -S capitaine-cursors --noconfirm --needed
 sudo pacman -S arc-gtk-theme --noconfirm --needed
-yaourt ardis-icon-theme-circle --noconfirm --needed
-yaourt otf-san-francisco-pro --noconfirm --needed
-yaourt nerd-fonts-melso
+yaourt otf-san-francisco-pro
+echo "*.font: SF Pro Display 12" >> $HOME/.Xresources
+yaourt numix-circle-icon-theme-git
+yaourt otf-san-francisco-mono
 
-
+sudo pacman -S zsh --noconfirm --needed
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+yaourt nerd-fonts-meslo
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "Set ZSH_THEME=powerlevel10k/powerlevel10k in your ~/.zshrc."
+echo "Set plugins=(zsh-autosuggestions) in your ~/.zshrc."
+echo "Set plugins=(zsh-syntax-highlighting) in your ~/.zshrc."
+echo "Set termite font SF Mono 12"
