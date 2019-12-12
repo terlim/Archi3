dae4248b92aff722a4376441fb7fa30c6e74c061
terlim <terlim@yandex.ru>
2019-12-12 20:29:32 +0300

    fix

---
+++ dotfiles/.emacs.d/init.el
@@ -0,0 +1,38 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
+(add-to-list 'load-path "~/.emacs.d/ergoemacs-mode")
+(require 'ergoemacs-mode)
+
+(setq show-paren-style 'expression)
+(show-paren-mode 2)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(global-linum-mode 1)
+(global-hl-line-mode 1)
+
+(scroll-bar-mode -1)
+
+
+(global-set-key (kbd "<f2>") 'bs-show)
+
+
+(fset 'yes-or-no-p 'y-or-n-p)
+
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (misterioso))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
